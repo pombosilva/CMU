2d28709c3e3d85a31c3dd36dc4fc69ec1fee0c62
jecsct <jetravassos2001@gmail.com>
2023-05-15 17:58:31 +0100

    [MAP FRAGMENT] Added some more code in MainActivity

--- app/src/main/java/pt/ulisboa/tecnico/cmov/project/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/project/MainActivity.java
@@ -1,6 +1,7 @@
 package pt.ulisboa.tecnico.cmov.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment fragment = new Map_Fragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
     }
 }
